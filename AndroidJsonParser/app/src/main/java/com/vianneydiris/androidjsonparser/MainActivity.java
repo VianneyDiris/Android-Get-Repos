1694808c0ba418455a3d87e82eb607fa63c93c84
VianneyDiris <vianney.diris@gmail.com>
2020-10-31 13:21:51 +0100

    add getReposClicked method

--- AndroidJsonParser/app/src/main/java/com/vianneydiris/androidjsonparser/MainActivity.java
+++ AndroidJsonParser/app/src/main/java/com/vianneydiris/androidjsonparser/MainActivity.java
@@ -73,44 +73,46 @@ public class MainActivity extends AppCompatActivity {
         // that expects a JSON Array Response.
         // To fully understand this, I'd recommend readng the office docs: https://developer.android.com/training/volley/index.html
         JsonArrayRequest arrReq = new JsonArrayRequest(Request.Method.GET, url,
-                new Response.Listener<JSONArray>() {
-                    @Override
-                    public void onResponse(JSONArray response) {
-                        // Check the length of our response (to see if the user has any repos)
-                        if (response.length() > 0) {
-                            // The user does have repos, so let's loop through them all.
-                            for (int i = 0; i < response.length(); i++) {
-                                try {
-                                    // For each repo, add a new line to our repo list.
-                                    JSONObject jsonObj = response.getJSONObject(i);
-                                    String repoName = jsonObj.get("name").toString();
-                                    String lastUpdated = jsonObj.get("updated_at").toString();
-                                    addToRepoList(repoName, lastUpdated);
-                                } catch (JSONException e) {
-                                    // If there is an error then output this to the logs.
-                                    Log.e("Volley", "Invalid JSON Object.");
-                                }
-
+                response -> {
+                    // Check the length of our response (to see if the user has any repos)
+                    if (response.length() > 0) {
+                        // The user does have repos, so let's loop through them all.
+                        for (int i = 0; i < response.length(); i++) {
+                            try {
+                                // For each repo, add a new line to our repo list.
+                                JSONObject jsonObj = response.getJSONObject(i);
+                                String repoName = jsonObj.get("name").toString();
+                                String lastUpdated = jsonObj.get("updated_at").toString();
+                                addToRepoList(repoName, lastUpdated);
+                            } catch (JSONException e) {
+                                // If there is an error then output this to the logs.
+                                Log.e("Volley", "Invalid JSON Object.");
                             }
-                        } else {
-                            // The user didn't have any repos.
-                            setRepoListText("No repos found.");
-                        }
 
+                        }
+                    } else {
+                        // The user didn't have any repos.
+                        setRepoListText("No repos found.");
                     }
+
                 },
 
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        // If there a HTTP error then add a note to our repo list.
-                        setRepoListText("Error while calling REST API");
-                        Log.e("Volley", error.toString());
-                    }
+                error -> {
+                    // If there a HTTP error then add a note to our repo list.
+                    setRepoListText("Error while calling REST API");
+                    Log.e("Volley", error.toString());
                 }
         );
         // Add the request we just defined to our request queue.
         // The request queue will automatically handle the request as soon as it can.
         requestQueue.add(arrReq);
+    }
+
+    public void getReposClicked(View v) {
+        // Clear the repo list (so we have a fresh screen to add to)
+        clearRepoList();
+        // Call our getRepoList() function that is defined above and pass in the
+        // text which has been entered into the etGitHubUser text input field.
+        getRepoList(etGitHubUser.getText().toString());
     }
 }
