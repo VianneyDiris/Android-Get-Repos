7dc081f1f8f23700d0eb82e3eed71a6a3598e001
VianneyDiris <vianney.diris@gmail.com>
2020-10-31 13:05:59 +0100

    add onCreate method

--- AndroidJsonParser/app/src/main/java/com/vianneydiris/androidjsonparser/MainActivity.java
+++ AndroidJsonParser/app/src/main/java/com/vianneydiris/androidjsonparser/MainActivity.java
@@ -1,14 +1,46 @@
 package com.vianneydiris.androidjsonparser;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
+import com.android.volley.Response;
+import com.android.volley.Request;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONException;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    EditText etGitHubUser; // This will be a reference to our GitHub username input.
+    Button btnGetRepos;  // This is a reference to the "Get Repos" button.
+    TextView tvRepoList;  // This will reference our repo list text box.
+    RequestQueue requestQueue;  // This is our requests queue to process our HTTP requests.
+
+    String baseUrl = "https://api.github.com/users/";  // This is the API base URL (GitHub API)
+    String url;  // This will hold the full URL which will include the username entered in the etGitHubUser.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState);  // This is some magic for Android to load a previously saved state for when you are switching between actvities.
+        setContentView(R.layout.activity_main);  // This links our code to our layout which we defined earlier.
+
+        this.etGitHubUser = (EditText) findViewById(R.id.et_github_user);  // Link our github user text box.
+        this.btnGetRepos = (Button) findViewById(R.id.btn_get_repos);  // Link our clicky button.
+        this.tvRepoList = (TextView) findViewById(R.id.tv_repo_list);  // Link our repository list text output box.
+        this.tvRepoList.setMovementMethod(new ScrollingMovementMethod());  // This makes our text box scrollable, for those big GitHub contributors with lots of repos :)
+
+        requestQueue = Volley.newRequestQueue(this);  // This setups up a new request queue which we will need to make HTTP requests.
     }
 }
